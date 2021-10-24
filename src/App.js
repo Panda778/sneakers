84a1ad5a347989026628e0978005d87788a8e74f
just_yan <artem200226dima18@gmail.com>
2021-10-24 10:44:34 +0500

    сделал смену картинок

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import './App.css';
-import Card from "./components/Card";
+import Card from "./components/Card/Card";
 import Header from "./components/Header";
 import Drawer from "./components/Drawer";
 
+
+const arr = [
+    {
+        title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+        price: 12990,
+        imgUrl: '/img/sneakers/1.jpg',
+        id: 1
+    },
+    {
+        title: 'Мужские Кроссовки Nike Air Max 270',
+        price: 17490,
+        imgUrl: '/img/sneakers/2.jpg',
+        id: 2
+    },
+    {
+        title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+        price: 14000,
+        imgUrl: '/img/sneakers/3.jpg',
+        id: 3
+    },
+    {
+        title: 'Кроссовки Puma X Aka Boku Future Raider',
+        price: 32220,
+        imgUrl: '/img/sneakers/4.jpg',
+        id: 4
+    }
+]
 function App() {
 
     return (
@@ -22,11 +49,14 @@ function App() {
                 </div>
 
                 <div className="d-flex">
-                    <Card/>
-                    <Card/>
-                    <Card/>
-                    <Card/>
+                    {arr.map((item)=>
+                        <Card
+                            key={item.id}
+                        title={item.title}
+                        price={item.price}
+                        imgUrl={item.imgUrl}
 
+                    />)}
                 </div>
 
             </div>
---
+++ src/components/Card/Card.js
@@ -0,0 +1,34 @@
+import styles from './Card.module.scss'
+import {useState} from "react";
+
+const Card = ({title, price, imgUrl}) => {
+
+    const [isAdded, setIsAdded] = useState(false)
+
+    const handleChange = () => {
+        setIsAdded(!isAdded)
+    }
+    console.log(isAdded)
+
+    return (
+        <div className={styles.card}>
+            <div className={styles.favourite}>
+                <img src="/img/heart-unlicked.svg" alt=""/>
+            </div>
+            <img width={133} height={112} src={imgUrl} alt=""/>
+            <h5>{title}</h5>
+            <div className={'d-flex justify-between align-center'}>
+                <div className={'d-flex flex-column '}>
+                    <span>price:</span>
+                    <b>{price}</b>
+                </div>
+
+                <img className={styles.plus} onClick={handleChange}
+                     src={isAdded == false ? './img/plus.svg' : './img/check-mark.svg'} alt=""/>
+
+            </div>
+        </div>
+    )
+}
+
+export default Card
